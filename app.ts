5a183f7ecb489dcba8f2476193df59daf7abec2c
Fran <francielle877@gmail.com>
2019-08-16 20:23:55 -0300

    Criação de classes

---
+++ app.ts
@@ -0,0 +1,16 @@
+class Aluno{
+
+       constructor(public nome:string){}
+       exibir(){
+
+           console.log("Nome:" + this.nome)
+
+       }
+}
+
+
+
+
+
+let ronqui = new Aluno("Rafael Ronqui")
+ronqui.exibir()
